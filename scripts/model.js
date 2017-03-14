442fbb768078b2bcdfeabc2d04617dd8311cec9b
William Madden <madden.william.d@gmail.com>
2017-03-14 16:23:32 -0700

    added scripts for static SPA starting point, added a model for what is now an article but will be a project model with all appropriate data, the model includes a constructor and appends several functions to its prototype that are used  in rendering to the view, for now each article contains its own logic for being rendered appropriatley. THe data is being sorced from blogarticlesJS in the asets folder and will be relplaced with JSON and the data from CoolKid Brendans portfolio. The next step is to import the HTML from the CF 301 lab 2 for my starting point and then connecting all the appropriate files to the view, from here I can change the CSS, change the layout and taylor the project from a blog into the Cool Kid Brendan , and possibley even eventually my own portfolio SPA

---
+++ scripts/model.js
@@ -0,0 +1,45 @@
+'use strict';
+var articles = [];
+
+function Article (opts) {
+  // TODO: DONE Use the object passed in to complete this constructor function:
+  // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+
+  /* use jQuery to fill in current template clone with properties from particular Article instance.*/
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
+  $newArticle.find('section.article-body').html(this.body); //THIS BREAKS THE PAGE!!!!!!!
+  /* TODO: This clone article is no longer a template,
+  as it now has real data attached to it! We need to account
+  for that before this current article gets rendered to our
+  DOM. */
+  $newArticle.removeAttr('class');
+
+  return $newArticle;
+};
+
+ourLocalData.sort(function(curElem, nextElem) {
+  return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
+});
